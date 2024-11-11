e1494e4e08f8d30bcb4939347afbff023619a9d5
Paula Oseroff <oseroffp@gmail.com>
2024-11-11 00:18:53 -0300

    feat: done

--- tp8/src/main.py
+++ tp8/src/main.py
@@ -1,11 +1,113 @@
 from ui import Ui_MainWindow
+import matplotlib
+matplotlib.use('QtAgg')
+
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 from PyQt6 import QtWidgets
+from logic import new_phantom, new_ellipse, add_ellipse, calculate_radon_transform, calculate_inverse_radon_transform
+from plots import plot_phantom, plot_radon_transform, plot_inverse_radon, plot_single_degree_radon_transform
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ellipses = dict()
+        self.draw_phantom()
+
+        self.ui.desdeLineEdit.setText("0")
+        self.ui.pasoLineEdit.setText("10")
+        self.ui.hastaLineEdit.setText("180")
+        self.calculate_radon_transform()
+
+        self.iradon_filters = {
+            "Ram-Lak": "ramp",
+            "Shepp-Logan": "shepp-logan",
+            "Cosine": "cosine",
+            "Hamming": "hamming",
+            "Hann": "hann",
+            "None": None
+        }
+
+        self.calculate_iradon_transform()
+
+        self.ui.agregarButton.clicked.connect(self.add_ellipse)
+        self.ui.borrarButton.clicked.connect(self.remove_ellipse)
+        self.ui.actualizarButton.clicked.connect(self.draw_phantom)
+
+        self.ui.calcularButton.clicked.connect(self.calculate_radon_transform)
+        self.ui.iCalcularButton.clicked.connect(self.calculate_iradon_transform)
+        self.ui.verButton.clicked.connect(self.draw_angle)
+        
+
+    def add_ellipse(self):
+        try:
+            I = float(self.ui.intensidadLineEdit.text())
+            A = float(self.ui.inclinacionLineEdit.text())
+            X = float(self.ui.semiEjeXLineEdit.text())
+            Y = float(self.ui.semiEjeYLineEdit.text())
+            CX = float(self.ui.centroXLineEdit.text())
+            CY = float(self.ui.centroYLineEdit.text())
+            ellipse = new_ellipse(self.phantom, I, A, X, Y, CX, CY)
+            ellipse_key = f"I={I}, X={X}, Y={Y}, Cx={CX}, Cy={CY}, A={A}"
+            self.ellipses[ellipse_key] = ellipse
+            self.ui.listWidget.addItem(ellipse_key)
+        except:
+            pass
+
+    def draw_phantom(self):
+        self.phantom = new_phantom()
+        for ellipse in self.ellipses.values():
+            self.phantom = add_ellipse(self.phantom, ellipse)
+        plot_phantom(self.ui.phantomCanvas.figure, self.phantom)
+        self.ui.phantomCanvas.draw()
+
+
+    def remove_ellipse(self):
+        idxs = self.ui.listWidget.selectedIndexes()
+        for idx in idxs:
+            item = self.ui.listWidget.takeItem(idx.row())
+            self.ellipses.pop(item.text())
+
+    def calculate_radon_transform(self):
+        try:
+            start = int(self.ui.desdeLineEdit.text())
+            step = int(self.ui.pasoLineEdit.text())
+            end = int(self.ui.hastaLineEdit.text())
+            radon_transform, theta = calculate_radon_transform(self.phantom, start, step, end)
+            self.radon_transform = radon_transform
+            self.theta = theta
+
+            self.ui.iDesdeLineEdit.setText(str(start))
+            self.ui.iPasoLineEdit.setText(str(step))
+            self.ui.hastaLineEdit_2.setText(str(end))
+
+            plot_radon_transform(self.ui.radonCanvas.figure, self.radon_transform, self.theta)
+            self.ui.radonCanvas.draw()
+        except:
+            pass
+
+    def calculate_iradon_transform(self):
+        try:
+            iradon_filter = self.ui.filtroComboBox.currentText()
+            iradon_interpolation = self.ui.interpolacionComboBox.currentText()
+
+            iphantom = calculate_inverse_radon_transform(self.radon_transform, self.theta, iradon_interpolation, self.iradon_filters[iradon_filter])
+            plot_inverse_radon(self.ui.iradonCanvas.figure, iphantom)
+            self.ui.iradonCanvas.draw()
+        except:
+            pass
+
+    def draw_angle(self):
+        try:
+            angle = int(self.ui.anguloLineEdit.text())
+            plot_single_degree_radon_transform(self.ui.radonCanvas.figure, self.radon_transform, angle, self.theta)
+            self.ui.radonCanvas.draw()
+        except:
+            pass
 
 
 if __name__ == "__main__":
--- tp8/src/plots.py
+++ tp8/src/plots.py
@@ -1,36 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_phantom(phantom: np.ndarray) -> plt.Figure:
-    fig = plt.Figure()
+def plot_phantom(fig: plt.Figure, phantom: np.ndarray) -> None:
+    fig.clear()
     axes = fig.gca()
     ax = axes.imshow(phantom, cmap=plt.get_cmap("hot"), vmax=1, vmin=0)
-    plt.colorbar(ax)
-    return fig
+    fig.colorbar(ax)
+    return None
 
-def plot_radon_transform(radon_transform: np.ndarray, theta: np.ndarray) -> plt.Figure:
-    fig = plt.Figure()
+def plot_radon_transform(fig: plt.Figure, radon_transform: np.ndarray, theta: np.ndarray) -> None:
+    fig.clear()
     axes = fig.gca()
+    radon_max = radon_transform.max()
+    radon_min = radon_transform.min()
     ax = axes.imshow(
         radon_transform,
         cmap=plt.cm.get_cmap("hot"),
         aspect='auto',
+        vmin=radon_min,
+        vmax=radon_max if radon_max != radon_min else 250,
         extent=[theta[0], theta[-1], 125, -125]
     )
-    plt.colorbar(ax)
-    return fig
+    fig.colorbar(ax)
+    return None
 
-def plot_single_degree_radon_transform(radon_transform: np.ndarray, degree: int, theta: np.ndarray) -> plt.Figure:
+def plot_single_degree_radon_transform(fig: plt.Figure, radon_transform: np.ndarray, degree: int, theta: np.ndarray) -> None:
     if degree not in theta:
         return None
     idx = np.where(theta == degree)
-    fig = plt.Figure()
+    fig.clear()
     axes = fig.gca()
     axes.plot(radon_transform[:, idx[0]])
-    return fig
+    return None
 
-def plot_inverse_radon(iphantom: np.ndarray) -> plt.Figure:
-    fig = plt.Figure()
+def plot_inverse_radon(fig: plt.Figure, iphantom: np.ndarray) -> None:
+    fig.clear()
     axes = fig.gca()
     ax = axes.imshow(
         iphantom,
@@ -39,5 +43,5 @@ def plot_inverse_radon(iphantom: np.ndarray) -> plt.Figure:
         vmin=0,
         vmax=1
     )
-    plt.colorbar(ax)
-    return fig
+    fig.colorbar(ax)
+    return None
